88b32c069321b94128c752a2a76bbda4b671ba20
sabrina-fontana <sabfon@hotmail.it>
2020-12-22 15:33:23 +0100

    modifiche

--- js/script.js
+++ js/script.js
@@ -34,22 +34,22 @@ button.addEventListener('click', function() {
         total += parseInt(ingredientsCheckboxes[i].value);
       }
     }
-  }
-  // controllo che l'utente selezioni almeno 2 ingredienti
-  if (counterIngredients < 2) {
-    ingredientsError.classList.add('ingredients-error-css');
-    ingredientsError.innerHTML = '<i class="fa fa-exclamation-triangle" aria-hidden="true"></i> Devi selezionare almeno 2 ingredienti';
-    totalPriceHTML.innerHTML = '€ ' + (10).toFixed(2);
-  } else {
-    ingredientsError.classList.remove('ingredients-error-css');
-    ingredientsError.style.display = 'none';
-    // se il codice sconto inserito è valido -> sconto 20%
-    for (var i = 0; i < coupon.length; i++) {
-      if (couponInserito.value === coupon[i]) {
-        total = total * 0.8;
+    // controllo che l'utente selezioni almeno 2 ingredienti
+    if (counterIngredients < 2) {
+      ingredientsError.classList.add('ingredients-error-css');
+      ingredientsError.innerHTML = '<i class="fa fa-exclamation-triangle" aria-hidden="true"></i> Devi selezionare almeno 2 ingredienti';
+      totalPriceHTML.innerHTML = '€ ' + (10).toFixed(2);
+    } else {
+      ingredientsError.classList.remove('ingredients-error-css');
+      ingredientsError.style.display = 'none';
+      // se il codice sconto inserito è valido -> sconto 20%
+      for (var i = 0; i < coupon.length; i++) {
+        if (couponInserito.value === coupon[i]) {
+          total = total * 0.8;
+        }
+        // else if (couponInserito.value.length > 1 && coupon.indexOf(couponInserito.value) === -1)
       }
-      // else if (couponInserito.value.length > 1 && coupon.indexOf(couponInserito.value) === -1)
     }
+    totalPriceHTML.innerHTML = '€ ' + total.toFixed(2);
   }
-  totalPriceHTML.innerHTML = '€ ' + total.toFixed(2);
 });
